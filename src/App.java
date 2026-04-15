80a38d3cbc421fc177e198e3216e4d3e2b2e5a82
domerodasmolina <domerodasmolina@gmail.com>
2026-04-15 10:28:10 -0500

    icc-est-u4-generador

--- src/App.java
+++ src/App.java
@@ -1,5 +1,43 @@
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int cantidadEstudiantes = 60_000_000;
+        int cantidadConsultas = 40_000;
+
+        System.out.println("Generando estudiantes ...");
+        
+        List<Estudiante> estudiantes = Generador.generarEstudiantes(cantidadEstudiantes);
+        //for (Estudiante est : estudiantes){
+        //    System.out.println(est.nombre);
+        //}
+        ///Buscar si existe el USR
+        /// CON CI 1_999_993
+        String cedulaBuscar = "51999993";
+        for (Estudiante est : estudiantes){
+            if(est.cedula.equals(cedulaBuscar)){
+                System.out.println("EXISTE");
+                break;
+            }
+        }
+        // int inicio = 0;
+        // int fin = estudiantes.size()-1;
+
+        // while(inicio<= fin){
+        //     int medio = (inicio+fin)/2;
+        //     Estudiante actual = estudiantes.get(medio);
+
+        //     int comparacion = actual.cedula.compareTo(cedulaBuscar);
+
+        //     if (comparacion ==0){
+        //         System.out.println("EXISTE");
+        //         break;
+        
+        //     } else if (comparacion <0){
+        //         inicio = medio+1;
+        //     }else{
+        //         fin = medio-1;
+        //     }
+        // }
     }
 }
---
+++ src/Generador.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generador {
+    public static List<Estudiante> generarEstudiantes(int cantidadEstudiantes){
+            
+        List<Estudiante> estudiantes = new ArrayList<>();
+        for (int i=0; i<cantidadEstudiantes;i++){
+            String cedula = i+"";
+            String nombre = "Estudiante_"+i;
+            Estudiante estudiante = new Estudiante(nombre, cedula);
+            estudiantes.add(estudiante);
+        }
+        return estudiantes;
+    }            
+
+}
+                                                                       
+
+
+
+
+
+
+
+
+
+
+
